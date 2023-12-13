c2f937b4da50009d69da200954babb85f00a1c51
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-12-13 14:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function makeid(l) {
   // write your code here
-	var text="";
-	var char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var res="";
+	var char_list="ABCDEFGHIJKLMNOPQSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 	for(var i=0;i<l;i++){
-		text+=char_list.charAt(
-			Math.floor(
-				Math.random()* char_list.length
+		res+= char_list.charAt(
+			Math.floor(	
+				Math.random()*char_list.length
 			)
 		)
 	}
-	return text;
+	return res;
 }
 // Do not change the code below.
 const l = prompt("Enter a number.");
